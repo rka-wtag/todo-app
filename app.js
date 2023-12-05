894704ea64655dda07c9d89c01b970ba6f5ffb9e
Riajul Kabir <riajul.kabir@welldev.io>
2023-12-05 17:48:30 +0600

    formatted code

--- app.js
+++ app.js
@@ -1,73 +1,64 @@
-
-import {
-    todoInputBox$,
-    todoList$,
-    todoSubmitButton$
-} from "./domElements.js";
-
+import { todoInputBox$, todoList$, todoSubmitButton$ } from "./domElements.js";
 
 let todos = [];
 
 const createInputField = (todo) => {
-    const inputElement = document.createElement('input');
-    inputElement.classList.add("text");
-    inputElement.type = "text";
-    inputElement.value = todo.text;
-    inputElement.setAttribute("readonly", "readonly");
+  const inputElement = document.createElement("input");
+  inputElement.classList.add("text");
+  inputElement.type = "text";
+  inputElement.value = todo.text;
+  inputElement.setAttribute("readonly", "readonly");
 
-    return inputElement;
-}
+  return inputElement;
+};
 
 const handleDelete = (todo) => {
-    todos = todos.filter(todoElement => todoElement.id !== todo.id);
-    renderTodos();
-}
+  todos = todos.filter((todoElement) => todoElement.id !== todo.id);
+  renderTodos();
+};
 
 const createDeleteButton = (todo) => {
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('todo-delete-button');
-    deleteButton.innerText = 'Delete';
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("todo-delete-button");
+  deleteButton.innerText = "Delete";
 
-    deleteButton.addEventListener('click', () => {
-        handleDelete(todo);
-    })
-    return deleteButton;
-
-}
-
+  deleteButton.addEventListener("click", () => {
+    handleDelete(todo);
+  });
+  return deleteButton;
+};
 
 const createElement = (todo) => {
-    const todo$ = document.createElement('li');
-    const inputElement = createInputField(todo);
-    const todoDeleteButton$ = createDeleteButton(todo);
-    todo$.appendChild(inputElement);
-    return todo$;
-}
+  const todo$ = document.createElement("li");
+  const inputElement = createInputField(todo);
+  const todoDeleteButton$ = createDeleteButton(todo);
+  todo$.appendChild(inputElement);
+  return todo$;
+};
 
 const handleAddTodo = (e) => {
-    e.preventDefault();
-    
-    if(!todoInputBox$.value){
-        alert('Please fill out the task');
-        return;
-    }
+  e.preventDefault();
 
-    const todoObj = {
-        id : Date.now(),
-        text : todoInputBox$.value,
-    };
+  if (!todoInputBox$.value) {
+    alert("Please fill out the task");
+    return;
+  }
 
-    todos.push(todoObj);
-    todoInputBox$.value = ""
-    renderTodos()
-   
-}
+  const todoObj = {
+    id: Date.now(),
+    text: todoInputBox$.value,
+  };
+
+  todos.push(todoObj);
+  todoInputBox$.value = "";
+  renderTodos();
+};
 
 const renderTodos = () => {
-    todoList$.innerHTML = null;
-    todos.forEach(todo => {
-        todoList$.appendChild(createElement(todo));
-    }) 
-}
+  todoList$.innerHTML = null;
+  todos.forEach((todo) => {
+    todoList$.appendChild(createElement(todo));
+  });
+};
 
-todoSubmitButton$.addEventListener('click', handleAddTodo)
+todoSubmitButton$.addEventListener("click", handleAddTodo);
